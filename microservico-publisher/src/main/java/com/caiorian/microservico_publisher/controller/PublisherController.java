0cbee41a2aa7b062dab8d1c2c5d02e1fa9bad4cc
CaioRian187 <caiorian246810@gmail.com>
2026-08-12 07:56:29 -0300

    fix-publisher: Refatorando a estrutura do publisher

--- microservico-publisher/src/main/java/com/caiorian/microservico_publisher/controller/PublisherController.java
+++ microservico-publisher/src/main/java/com/caiorian/microservico_publisher/controller/PublisherController.java
@@ -3,25 +3,20 @@ package com.caiorian.microservico_publisher.controller;
 import com.caiorian.microservico_publisher.service.PublisherService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/publisher")
 @RequiredArgsConstructor
 public class PublisherController {
 
-    @Value("${rabbitmq.queuename}")
-    private String queueName;
-
     private final PublisherService publisherService;
 
     @PostMapping("/message")
-    public void publisherMessage(@RequestBody String message){
-        System.out.println("Message: " + message);
-
-        this.publisherService.publisherMessageQueue(message, queueName);
+    public ResponseEntity<String> publisherMessage(@RequestParam("message") String message){
+        this.publisherService.publisherMessageQueue(message);
+        return ResponseEntity.status(HttpStatus.OK).body("Mensagem enviada com sucesso: " + message);
     }
 }
--- microservico-publisher/src/main/java/com/caiorian/microservico_publisher/service/PublisherService.java
+++ microservico-publisher/src/main/java/com/caiorian/microservico_publisher/service/PublisherService.java
@@ -2,15 +2,19 @@ package com.caiorian.microservico_publisher.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PublisherService {
 
+    @Value("${rabbitmq.queuename}")
+    private String queueName;
+
     private final RabbitTemplate rabbitTemplate;
 
-    public void publisherMessageQueue(String queueName, String message){
+    public void publisherMessageQueue(String message){
         System.out.println(message);
         // Passando a mensagem e o nome da fila para o RabbitMQ
         rabbitTemplate.convertAndSend(queueName, message);
